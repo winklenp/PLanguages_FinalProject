8de1ca00b0570420cb6c5fd32d0fbef5dc62cf70
winklenp <55988321+winklenp@users.noreply.github.com>
2020-12-02 16:41:51 -0500

    Add files via upload

--- central_control.bat
+++ central_control.bat
@@ -10,15 +10,21 @@ for /f %%i in ("quit.txt") do set size=$$~zi
         for /f %%i in ("start_form.txt") do set size=%%~zi
         if %size% gtr 0 python main.py
 
+        echo "" > start_form.txt
+
         set size=0
 
         for /f %%i in ("start_prolog.txt") do set size=%%~zi
         if %size% gtr 0 prolog.p1
 
+        echo "" > start_prolog.txt
+
         set size=0
 
         for /f %%i in ("start_haskell.txt") do set size=%%~zi
         if %size% gtr 0 haskell.h1
+
+        echo "" > start_haskell.txt
 
         set size=0
         goto:while
