16d250b4f33069e168f9c98ee2d62a8568a7eb1d
winklenp <55988321+winklenp@users.noreply.github.com>
2020-12-02 16:38:34 -0500

    Add files via upload

--- central_control.bat
+++ central_control.bat
@@ -1,6 +1,28 @@
-covidspread.exe
+Covid_Spread.exe
 
 set size=0
-for /f %%i in ("start_form.txt") do set size=%%~zi
-if %size% gtr 0 python main.py
+:while
+for /f %%i in ("quit.txt") do set size=$$~zi
+    if %size%==0
+        (
+        set size=0
+
+        for /f %%i in ("start_form.txt") do set size=%%~zi
+        if %size% gtr 0 python main.py
+
+        set size=0
+
+        for /f %%i in ("start_prolog.txt") do set size=%%~zi
+        if %size% gtr 0 prolog.p1
+
+        set size=0
+
+        for /f %%i in ("start_haskell.txt") do set size=%%~zi
+        if %size% gtr 0 haskell.h1
+
+        set size=0
+        goto:while
+        )
+    else goto:eof
+
 
